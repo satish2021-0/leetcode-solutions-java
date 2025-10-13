2d991bada896f49e8f900a8eed0a51d482ffd3aa
SATISH KUMAR <satishkr0924@gmail.com>
2025-10-13 08:29:01 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 40.9 MB (99.99%)

--- leetcode/0342-power-of-four/solution.java
+++ leetcode/0342-power-of-four/solution.java
@@ -1,13 +1,10 @@
 class Solution {
     public boolean isPowerOfFour(int n) {
-         if(n<=0)
-                 return false;
-           while(n>1){
-            int bit = n%4;
-            if(bit!=0)
-              return false;
-              n/=4;
-    }
-    return true;
+         if (n==1)
+           return true;
+           if(n==0 || n%4!=0)
+                    return false;
+
+                return isPowerOfFour(n/4);
 }
 }
