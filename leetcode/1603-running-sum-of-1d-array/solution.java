2a5273e31abeaa2a44a443a41ccba34f74dc577c
SATISH KUMAR <satishkr0924@gmail.com>
2025-12-08 18:09:01 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 44.1 MB (78.48%)

--- leetcode/1603-running-sum-of-1d-array/solution.java
+++ leetcode/1603-running-sum-of-1d-array/solution.java
@@ -1,7 +1,7 @@
 class Solution {
     public int[] runningSum(int[] nums) {
-        int sum=0;
-        for(int i=1; i<nums.length;i++){
+
+        for(int i=1;i<nums.length;i++){
             nums[i]=nums[i]+nums[i-1];
         }
         return nums;
