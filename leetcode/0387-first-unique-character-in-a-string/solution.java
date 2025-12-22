ff009a44712285b21f3192234309a2f3d17d703d
SATISH KUMAR <satishkr0924@gmail.com>
2025-12-22 15:00:43 +0000

    Sync LeetCode submission Runtime - 5 ms (97.18%), Memory - 47 MB (59.66%)

---
+++ leetcode/0387-first-unique-character-in-a-string/solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int firstUniqChar(String s) {
+        int [] freq = new int[26];
+        for(int i =0;i<s.length();i++){
+            char c= s.charAt(i);
+            freq[c-97]++;
+        }
+
+        for(int i=0;i<s.length();i++){
+            if(freq[s.charAt(i)-97]==1)
+                return i;
+        }
+
+    return -1;
+    }
+}
