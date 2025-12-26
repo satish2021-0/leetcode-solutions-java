223cccfa53ce62eb24180eb9853ff6d82016377b
SATISH KUMAR <satishkr0924@gmail.com>
2025-12-26 05:13:35 +0000

    Sync LeetCode submission Runtime - 3 ms (90.21%), Memory - 44.4 MB (79.10%)

--- leetcode/0242-valid-anagram/solution.java
+++ leetcode/0242-valid-anagram/solution.java
@@ -1,19 +1,23 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-      int [] freq1=Frequency(s);
-        int [] freq2=Frequency(t);
-        for(int i=0;i<26;i++){
+
+         int [] freq1 = Freq(s);
+         int [] freq2 = Freq(t);
+
+         for(int i =0;i<26;i++){
             if(freq1[i]!=freq2[i])
-                return false;
-        }
+                    return false;
+         }
         return true;
     }
-    public static int[] Frequency(String s){
+
+    public static int [] Freq(String s){
         int [] freq = new int[26];
         for(int i=0;i<s.length();i++){
-            char c= s.charAt(i);
+            char c = s.charAt(i);
             freq[c-97]++;
         }
         return freq;
     }
-}
+
+    }
