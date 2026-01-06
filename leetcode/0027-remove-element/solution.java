a5c6e9b067d1a649d85dbac091fce3b8107e08af
SATISH KUMAR <satishkr0924@gmail.com>
2026-01-06 18:37:40 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 43.8 MB (13.63%)

--- leetcode/0027-remove-element/solution.java
+++ leetcode/0027-remove-element/solution.java
@@ -6,13 +6,9 @@ class Solution {
          if (x != val)
             s1.add(x);
        } 
-
-       
-
         for(int i=0; i<s1.size();i++){
            nums[i] = s1.get(i);
         }
-
     return s1.size();
     }
 }
