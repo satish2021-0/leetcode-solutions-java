b09383ec5e3b8f14f91a31ec557989e7a6e3ede4
SATISH KUMAR <satishkr0924@gmail.com>
2026-01-06 18:00:05 +0000

    Sync LeetCode submission Runtime - 1 ms (75.54%), Memory - 47.1 MB (14.00%)

---
+++ leetcode/0026-remove-duplicates-from-sorted-array/solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int count =1;
+
+        for(int i=0,j=1;j<nums.length;j++){
+            if(nums[i]!=nums[j]){
+                i++;
+                nums[i]=nums[j];
+                count++;
+            }
+        }
+    return count;
+    }
+}
