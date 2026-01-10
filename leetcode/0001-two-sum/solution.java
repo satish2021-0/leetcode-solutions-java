dfa90b56e402982ceb041aa34bc92cfa2ef6d9af
SATISH KUMAR <satishkr0924@gmail.com>
2026-01-10 18:25:38 +0000

    Sync LeetCode submission Runtime - 44 ms (34.80%), Memory - 47.4 MB (5.85%)

--- leetcode/0001-two-sum/solution.java
+++ leetcode/0001-two-sum/solution.java
@@ -6,7 +6,6 @@ class Solution {
             for(int j=i+1;j<nums.length;j++){
                 if(nums[i]+ nums[j] == target)
                     return new int []{i,j};
-
             }
         }
         return new int []{};
