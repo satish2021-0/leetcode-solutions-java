4d4ca60c14a9547ebe5dbc88eca7c92c2bd78bec
SATISH KUMAR <satishkr0924@gmail.com>
2026-01-10 19:14:30 +0000

    Sync LeetCode submission Runtime - 3 ms (59.45%), Memory - 47.3 MB (27.49%)

--- leetcode/0001-two-sum/solution.java
+++ leetcode/0001-two-sum/solution.java
@@ -1,13 +1,21 @@
+import java.util.NoSuchElementException;
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        
-        for(int i =0; i<nums.length;i++){
-            
-            for(int j=i+1;j<nums.length;j++){
-                if(nums[i]+ nums[j] == target)
-                    return new int []{j,i};
-            }
-        }
-        return new int []{};
+
+       Map<Integer, Integer> m1 = new HashMap<>();
+
+       for(int i=0; i<nums.length;i++){
+
+        int temp = target-nums[i];
+
+        int index= m1.getOrDefault(temp,-1);
+
+        if(index>=0)
+            return new int []{i,index};
+
+        m1.put(nums[i],i);
+
+       }
+       throw new NoSuchElementException("Element is not present");
     }
 }
