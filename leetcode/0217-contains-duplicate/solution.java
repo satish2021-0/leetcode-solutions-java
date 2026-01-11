03b558d4501d6226abdd476e2a1107dfe944acd6
SATISH KUMAR <satishkr0924@gmail.com>
2026-01-11 08:39:41 +0000

    Sync LeetCode submission Runtime - 15 ms (64.29%), Memory - 98 MB (5.89%)

--- leetcode/0217-contains-duplicate/solution.java
+++ leetcode/0217-contains-duplicate/solution.java
@@ -1,14 +1,14 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
         
-        Set<Integer> m1 = new HashSet<>();
+        Map<Integer, Integer> m1 = new HashMap<>();
 
         for(int i=0;i<nums.length;i++){
 
-            if(m1.contains(nums[i]))
+            if(m1.containsKey(nums[i]))
                 return true;
 
-            m1.add(nums[i]);
+            m1.put(nums[i],i);
         }
         return false;
     }
