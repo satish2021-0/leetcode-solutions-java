3150f185d836854279129b6eb7326f6694cf7cc1
SATISH KUMAR <satishkr0924@gmail.com>
2026-01-12 08:34:45 +0000

    Sync LeetCode submission Runtime - 3 ms (59.45%), Memory - 47.4 MB (5.85%)

--- leetcode/0001-two-sum/solution.java
+++ leetcode/0001-two-sum/solution.java
@@ -1,28 +1,17 @@
-import java.util.*;
 class Solution {
-    public int[] twoSum(int[] nums, int target) throws NoSuchElementFoundException {
-
-       Map<Integer, Integer> m1 = new HashMap<>();
-
-       for(int i=0; i<nums.length;i++){
-
-        int temp = target-nums[i];
-
-        int index= m1.getOrDefault(temp,-1);
-
-        if(index>=0)
-            return new int []{i,index};
-
-        m1.put(nums[i],i);
-
-       }
-       throw new NoSuchElementFoundException("Element is not present");
+    public int[] twoSum(int[] nums, int target) {
+        
+        Map<Integer, Integer> m1 = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            int temp = target-nums[i];
+            int index= m1.getOrDefault(temp,-1);
+            if(index>=0)
+                return new int []{i,index};
+            m1.put(nums[i],i);
+        }
+    return new int []{};
     }
 }
 
-class NoSuchElementFoundException extends RuntimeException {
 
-    public NoSuchElementFoundException(String message) {
-        super(message);
-    }
-}
+
