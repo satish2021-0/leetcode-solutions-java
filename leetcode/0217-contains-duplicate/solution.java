87d97ce34b0206870dece3f9c30e35179172d291
SATISH KUMAR <satishkr0924@gmail.com>
2026-01-15 10:21:22 +0000

    Sync LeetCode submission Runtime - 15 ms (64.29%), Memory - 93.8 MB (30.16%)

--- leetcode/0217-contains-duplicate/solution.java
+++ leetcode/0217-contains-duplicate/solution.java
@@ -1,31 +1,29 @@
 import java.util.*;
-class Solution {
-    public boolean containsDuplicate(int[] nums) {
-        Map<Integer, Integer> m1 = new HashMap<>();
-      
-        for(int i=0;i<nums.length;i++){
-            if(m1.containsKey(nums[i]))
-                return true;
-            else{
-                m1.put(nums[i],m1.getOrDefault(nums[i],0) +1);
-            }
-        }
-        return false;
-    }
-}
-
+//Using Map
 // class Solution {
 //     public boolean containsDuplicate(int[] nums) {
-        
-//         Set<Integer> m1 = new HashSet<>();
-
+//         Map<Integer, Integer> m1 = new HashMap<>();
 //         for(int i=0;i<nums.length;i++){
-
-//             if(m1.contains(nums[i]))
+//             if(m1.containsKey(nums[i]))
 //                 return true;
-
-//             m1.add(nums[i]);
+//             else{
+//                 m1.put(nums[i],m1.getOrDefault(nums[i],0) +1);
+//             }
 //         }
 //         return false;
 //     }
 // }
+
+
+//Using Set
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> m1 = new HashSet<>();
+        for(int i=0;i<nums.length;i++){
+            if(m1.contains(nums[i]))
+                return true;
+            m1.add(nums[i]);
+        }
+        return false;
+    }
+}
