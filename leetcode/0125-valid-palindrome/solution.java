39055595cbeb3d3a6a7ff1ac181207a5aebb7b0d
SATISH KUMAR <satishkr0924@gmail.com>
2026-01-15 07:17:34 +0000

    Sync LeetCode submission Runtime - 256 ms (5.18%), Memory - 53.7 MB (5.11%)

--- leetcode/0125-valid-palindrome/solution.java
+++ leetcode/0125-valid-palindrome/solution.java
@@ -1,21 +1,24 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        s = s.toLowerCase();
-        String p = "";
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            if ((c >= 'a' && c <= 'z') || (c >= '0' && c <= '9')) {
-                p = p + c;
-            }
-        }
+        
+    s= s.toLowerCase().trim();
 
-        String q = "";
-        for (int i = 0; i<p.length(); i++) {
-            char z = p.charAt(i);
-            q = z + q;
+    String p ="";
+    for(int i=0; i<s.length();i++){
+        char c = s.charAt(i);
+        if((c>='a' && c<='z') || (c>='0' && c<='9')){
+            p=p+c;
         }
-        if (p.equals(q))
-            return true;
-        return false;
+    }
+
+    String q="";
+    for(int j=0;j<p.length();j++){
+         char x = p.charAt(j);
+         q=x+q;
+    }
+
+    if(p.equals(q))
+        return true;
+    return false;
     }
 }
