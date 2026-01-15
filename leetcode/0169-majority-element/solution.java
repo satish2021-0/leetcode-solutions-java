aba251fec5a57b3f269853431308c5cf0d380149
SATISH KUMAR <satishkr0924@gmail.com>
2026-01-15 10:07:23 +0000

    Sync LeetCode submission Runtime - 13 ms (30.11%), Memory - 52.7 MB (77.64%)

--- leetcode/0169-majority-element/solution.java
+++ leetcode/0169-majority-element/solution.java
@@ -1,26 +1,3 @@
-// class Solution {
-//     public int majorityElement(int[] nums) {
-//         int maj=nums.length/2;
-//         int max=nums[0];
-//         int min=nums[0];
-//         for(int x:nums){
-//             if(x>max)
-//                 max=x;
-//             else if(x<max)
-//                 min=x;
-//         }
-//         int [] freq = new int[max-min+1];
-//         for(int i=0; i<nums.length;i++){
-//             freq[nums[i]-min]++;
-//         }
-
-//         for(int i=0;i<freq.length;i++){
-//             if(freq[i]>maj)
-//                 return i+min;
-//         }
-//     return 0;
-//     }
-// }
 
 import java.util.*;
 
@@ -40,3 +17,31 @@ class Solution {
         return 0;
     }
 }
+
+
+//  Majority Element me frequency array approach memory limit exceed ho jayega kyunki array size values ke range par depend karta hai, jo bahut bada ho sakta hai. ***Frequency array tabhi use karo jab values ka range small aur fixed ho***.
+
+// class Solution {
+//     public int majorityElement(int[] nums) {
+//         int maj=nums.length/2;
+//         int max=nums[0];
+//         int min=nums[0];
+//         for(int x:nums){
+//             if(x>max)
+//                 max=x;
+//             else if(x<min)
+//                 min=x;
+//         }
+//         int [] freq = new int[max-min+1];
+//         for(int i=0; i<nums.length;i++){
+//             freq[nums[i]-min]++;
+//         }
+
+//         for(int i=0;i<freq.length;i++){
+//             if(freq[i]>maj)
+//                 return i+min;
+//         }
+//     return 0;
+//     }
+// }
+
