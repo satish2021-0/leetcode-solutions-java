14cba82c3f5b9bcc5cdc8a779d80cdc0a1f14f36
SATISH KUMAR <satishkr0924@gmail.com>
2026-01-20 13:26:31 +0000

    Sync LeetCode submission Runtime - 7 ms (35.41%), Memory - 46.2 MB (54.53%)

---
+++ leetcode/0003-longest-substring-without-repeating-characters/solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        
+        int left=0;
+        int maxlength=0;
+        Set<Character> set1= new HashSet<>();
+        for(int right=0; right<s.length();right++){
+            char ch= s.charAt(right);
+
+            while(set1.contains(ch)){
+                set1.remove(s.charAt(left));
+                left++;
+            }
+            set1.add(ch);
+            maxlength= Math.max(maxlength, right-left+1);
+        }
+    return maxlength;
+    }
+}
